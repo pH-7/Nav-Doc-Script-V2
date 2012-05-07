24d7baaf91b6b4fc48e976270ecc6865f2324bda
Pierre-Henry SORIA <ph_7@hotmail.be>
2012-05-07 16:26:27 +0200

    Change a comment

--- inc/conf.const.php
+++ inc/conf.const.php
@@ -9,10 +9,18 @@
 namespace PH7\Doc;
 defined('PH7') or exit('Restricted access');
 
+#################### VARIABLES ####################
+
+
+########## URL ##########
+
 $sHttp = (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']=='on')) ? 'https://' : 'http://';
 $sPhp_self = dirname(htmlspecialchars($_SERVER['PHP_SELF']));
 
-#################### URL ####################
+#################### CONSTANTS ####################
+
+
+########## URL ##########
 
 define('SELF', (substr($sPhp_self,-1) !== '/') ? $sPhp_self . '/' : $sPhp_self);
 define('RELATIVE', SELF);
@@ -23,12 +31,12 @@ define('PROT_URL', $sHttp);
 define('ROOT_PATH', dirname(__DIR__) . '/');
 define('DATA_PATH', ROOT_PATH . 'data/');
 
-#################### PATH ####################
+########## PATH ##########
 
 define('ROOT_URL', PROT_URL . $_SERVER['HTTP_HOST'] . SELF);
 define('STATIC_URL', RELATIVE . 'static/');
 
-#################### INFORMATION ####################
+########## INFORMATION ##########
 
 define('SITE_NAME', 'pH');
 define('SITE_SLOGAN', 'Documentation');
